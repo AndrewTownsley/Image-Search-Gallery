d91c8d9fc4ac30b0a2ae72e2bd4e848fceb21efa
Andrew TOwnsley <atownsley86@gmail.com>
2021-04-10 23:45:39 -0600

    Basic grid layout done for photo-container.  Photos link to their respective Pexels page.  Next is finding a way to display the Photographer with the image.

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 const query = document.getElementById('search');
 const searchBtn = document.getElementById('searchBtn');
-const container = document.getElementById('photo-container');
-let card;
+const photoContainer = document.getElementById('photo-container');
+const main = document.getElementById('main');
+let photoCard;
 
+const photoDiv = document.createElement('div');
+photoDiv.classList.add('photo-container');
+main.appendChild(photoDiv);
 
 searchBtn.addEventListener('click', () => {
-
-container.innerHTML = '';
+  
+  photoDiv.innerHTML = '';
 
   function getPhotos(images) {
     images.map(image => {
-      card = `<div class="photo-card">
+      photoCard = 
+      `<div class="photo-card">
+      <a href=${image.url}>
       <img src=${image.src.tiny} />
-    </div>`
-    container.innerHTML += card;
+      </a>
+      </div>`
+    photoDiv.innerHTML += photoCard;
+    console.log(image.photographer);
     })
   }
 
   fetch(`https://api.pexels.com/v1/search?query=${query.value}`, {
     headers: {
-      Authorization:  "563492ad6f91700001000001f58e6def40a2436c823881c0b23a45b7"
+      Authorization:  ""
     }
   })
   .then((response) => {
@@ -28,6 +36,8 @@ container.innerHTML = '';
   })
   .then(data => {
     getPhotos(data.photos)
+    console.log(data.photos);
+    console.log(data.photos.photographer);
   })
 })
 
