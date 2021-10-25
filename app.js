005ee57c003883c16ff3073fb7679ff3fd6691b4
Andrew TOwnsley <atownsley86@gmail.com>
2021-10-25 17:50:12 -0600

    Load More photos is working, however the searchPhotos returns the same photos that were originally fetched.

--- app.js
+++ app.js
@@ -1,35 +1,75 @@
 
-const query = document.getElementById('search');
+const queryInput = document.getElementById('search');
 const searchBtn = document.getElementById('searchBtn');
 const photoContainer = document.getElementById('photo-container');
 const main = document.getElementById('main');
 const photoHeader = document.getElementById('photo-header');
 let photoCard;
+let search = false;
+let pageNum = 1;
 
 const photoDiv = document.createElement('div');
 photoDiv.classList.add('photo-container');
 main.appendChild(photoDiv);
 
+const loadMoreBtn = document.createElement('button');
+loadMoreBtn.classList.add('load-btn');
+loadMoreBtn.classList.add('btn');
+loadMoreBtn.innerText = 'Load More'
+main.appendChild(loadMoreBtn);
+
+const fetchCuratedPhotos = async (pageNum) => {
+  const data = await fetch(`https://api.pexels.com/v1/curated?page=${pageNum}`,
+   {
+     headers: {
+       Authorization: "563492ad6f91700001000001f58e6def40a2436c823881c0b23a45b7",
+     },
+   });
+   const response = await data.json();
+   console.log("curated Photos:",response);
+   getPhotos(response);
+}
+
+
+function getPhotos(response) {
+  console.log(response);
+  response.photos.map(image => {
+    photoCard = 
+    `<div class="photo-card">
+    <a href=${image.url}>
+    <div class="photo-card-overlay">${image.photographer}</div>
+    <img src=${image.src.tiny} class="image"/>
+    </a>
+    </div>`
+  photoDiv.innerHTML += photoCard;
+  })
+}
+
+const searchPhotos = async (pageNum) => {
+  const data = await fetch(`https://api.pexels.com/v1/search?query=${queryInput.value}&page=${pageNum}`,
+  {
+    headers: {
+      Authorization: "563492ad6f91700001000001f58e6def40a2436c823881c0b23a45b7",
+    },
+  });
+  const response = await data.json();
+  console.log("search Photos:", response);
+  getPhotos(response);
+}
+
+
+
+
 searchBtn.addEventListener('click', (e) => {
 
   e.preventDefault();
-  photoHeader.innerText = `'${query.value}'`;
+  let query = e.target.value;
+  search = true;
+  photoHeader.innerText = `'${queryInput.value}'`;
   photoDiv.innerHTML = '';
 
-  function getPhotos(images) {
-    images.map(image => {
-      photoCard = 
-      `<div class="photo-card">
-      <a href=${image.url}>
-      <div class="photo-card-overlay">${image.photographer}</div>
-      <img src=${image.src.tiny} alt=${query.value} class="image"/>
-      </a>
-      </div>`
-    photoDiv.innerHTML += photoCard;
-    })
-  }
 
-  fetch(`https://api.pexels.com/v1/search?query=${query.value}`, {
+  fetch(`https://api.pexels.com/v1/search?query=${queryInput.value}&page=${pageNum}`, {
     headers: {
       Authorization: "563492ad6f91700001000001f58e6def40a2436c823881c0b23a45b7"
     }
@@ -37,9 +77,20 @@ searchBtn.addEventListener('click', (e) => {
   .then((response) => {
     return response.json();
   })
-  .then(data => {
-    getPhotos(data.photos)
-    console.log(data.photos);
-  })
+  searchPhotos(query, pageNum);
 })
+
+loadMoreBtn.addEventListener('click', () => {
+  if(!search) {
+    pageNum++;
+    fetchCuratedPhotos(pageNum);
+  } else {
+    if(queryInput.value === "")
+    return;
+    pageNum++
+    searchPhotos(queryInput, pageNum);
+  }
+})
+
+fetchCuratedPhotos(pageNum);
 
