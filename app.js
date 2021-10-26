05e77b4155528a068cea26f11a9e7b456dab300b
Andrew TOwnsley <atownsley86@gmail.com>
2021-10-25 21:10:30 -0600

    Done, ready to be presented in its current state.  Come back later to add more functionality.

--- app.js
+++ app.js
@@ -11,14 +11,19 @@ const photoDiv = document.createElement('div');
 photoDiv.classList.add('photo-container');
 main.appendChild(photoDiv);
 
+const loadBtnDiv = document.createElement('div');
+loadBtnDiv.classList.add('load-more-div');
+main.appendChild(loadBtnDiv);
+
+
 const loadMoreBtn = document.createElement('button');
 loadMoreBtn.classList.add('load-btn');
 loadMoreBtn.classList.add('btn');
 loadMoreBtn.innerText = 'Load More'
-main.appendChild(loadMoreBtn);
+loadBtnDiv.appendChild(loadMoreBtn);
 
 const fetchCuratedPhotos = async (pageNum) => {
-  const data = await fetch(`https://api.pexels.com/v1/curated?page=${pageNum}`,
+  const data = await fetch(`https://api.pexels.com/v1/search?query=nature&page=${pageNum}`,
    {
      headers: {
        Authorization: "563492ad6f91700001000001f58e6def40a2436c823881c0b23a45b7",
@@ -55,23 +60,27 @@ const searchPhotos = async (pageNum) => {
 searchBtn.addEventListener('click', (e) => {
 
   e.preventDefault();
-  let query = e.target.value;
-  search = true;
-  photoHeader.innerText = `'${queryInput.value}'`;
-  photoDiv.innerHTML = '';
 
-  fetch(`https://api.pexels.com/v1/search?query=${queryInput.value}&page=${pageNum}`, {
-    headers: {
-      Authorization: "563492ad6f91700001000001f58e6def40a2436c823881c0b23a45b7"
-    }
-  })
-  .then((response) => {
-    return response.json();
-  })
-  searchPhotos(query, pageNum);
-})
+  if(queryInput.value !== '') {
 
-loadMoreBtn.addEventListener('click', () => {
+    let query = e.target.value;
+    search = true;
+    photoHeader.innerText = `'${queryInput.value}'`;
+    photoDiv.innerHTML = '';
+    
+    fetch(`https://api.pexels.com/v1/search?query=${queryInput.value}&page=${pageNum}`, {
+      headers: {
+        Authorization: "563492ad6f91700001000001f58e6def40a2436c823881c0b23a45b7"
+      }
+    })
+    .then((response) => {
+      return response.json();
+    })
+    searchPhotos(query, pageNum);
+  }
+  })
+  
+  loadMoreBtn.addEventListener('click', () => {
   if(!search) {
     pageNum++;
     fetchCuratedPhotos(pageNum);
